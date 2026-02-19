a69dc6328a3cd5cd54e130149066eae3a74181b1
Annalycia Melendez <melend16@purdue.edu>
2026-02-19 17:45:05 -0500

    This is what i'm working on currently, im taking a break now.

--- src/Signin.js
+++ src/Signin.js
@@ -1,10 +1,17 @@
 const logo =  document.getElementsByClassName('logo')[0];
 const block = document.getElementsByClassName('signin_background')[0];
 let logo_width = logo.offsetWidth;
+let logo_height = logo.offsetHeight;
+
 block.style.width = logo_width + 'px';
+block.style.height = 3 * logo_height + 'px';
 
 window.addEventListener('resize', () => {
     logo_width = logo.offsetWidth;
     block.style.width = logo_width + 'px';
+
+    logo_height = logo.offsetHeight;
+    block.style.height = 3 * logo_height + 'px';
+
 })
 
