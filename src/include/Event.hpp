6f4bf958eaabf44acce39e6b9222a1bb8f3bb960
Player48 <139013669+Player48@users.noreply.github.com>
2026-02-20 10:20:05 -0500

    Update Event.hpp

--- src/include/Event.hpp
+++ src/include/Event.hpp
@@ -16,5 +16,23 @@ class Event{
         time_t end_time;
 
     public:
-        
-}
+        Event(std::string creator_id,
+            std::vector<std::string> attendee_ids,
+            Location location_id,
+            time_t start_time,
+            time_t end_time);
+
+        std::string get_creator_id();
+        std::string get_id();
+        std::vector<std::string> get_attendee_ids();
+        Location get_locations();
+        time_t get_start_time();
+        time_t get_end_time();
+
+        void set_creator_id();
+        void set_id();
+        void set_attendee_ids();
+        void set_location_id();
+        void set_start_time();
+        void set_end_time();
+};       
