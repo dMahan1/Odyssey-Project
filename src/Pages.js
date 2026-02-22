bfa81f2ec6b8c1a12f2033ee64d1c7c52b518fb9
Annalycia Melendez <melend16@purdue.edu>
2026-02-22 13:01:35 -0500

    Added nav bar buttons and color changes when they are hovered over or clicked.

--- src/Pages.js
+++ src/Pages.js
@@ -1,9 +1,29 @@
 const nav_bar = document.getElementById('nav_bar');
 let window_height = window.innerHeight;
+let window_width = window.innerWidth;
+const calendar_button = document.getElementById('calendar_button');
+const map_button = document.getElementById('map_button')
+const settings_button = document.getElementById('settings_button');
 
 nav_bar.style.height = window_height / 8 + "px";
+nav_bar.style.gap = window_width / 8 + "px";
+change_proportions(calendar_button);
+change_proportions(map_button);
+change_proportions(settings_button);
 
 window.addEventListener ('resize', () => {
     window_height = window.innerHeight;
+    window_width = window.innerWidth;
+    
     nav_bar.style.height = window_height / 8 + 'px';
+    nav_bar.style.gap = window_width / 8 + "px";
+
+    change_proportions(calendar_button);
+    change_proportions(map_button);
+    change_proportions(settings_button);
 })
+
+function change_proportions(button) {
+    button.style.height = window_height / 8 -  + 'px';
+    button.style.width = window_width / 4 + "px";
+}
