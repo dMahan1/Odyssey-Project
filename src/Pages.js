9b7dc1b84cb88a511d35e42bb266e67ef3a91221
Annalycia Melendez <melend16@purdue.edu>
2026-02-22 13:19:35 -0500

    Changed some more on settings page, taking a break from this page to work on popups.

--- src/Pages.js
+++ src/Pages.js
@@ -4,12 +4,14 @@ let window_width = window.innerWidth;
 const calendar_button = document.getElementById('calendar_button');
 const map_button = document.getElementById('map_button')
 const settings_button = document.getElementById('settings_button');
+const friends_bar = document.getElementById('friends_bar');
 
 nav_bar.style.height = window_height / 8 + "px";
 nav_bar.style.gap = window_width / 8 + "px";
-change_proportions(calendar_button);
-change_proportions(map_button);
-change_proportions(settings_button);
+change_buttons(calendar_button);
+change_buttons(map_button);
+change_buttons(settings_button);
+friends_bar.style.height = window_height / 2 + "px";
 
 window.addEventListener ('resize', () => {
     window_height = window.innerHeight;
@@ -18,12 +20,15 @@ window.addEventListener ('resize', () => {
     nav_bar.style.height = window_height / 8 + 'px';
     nav_bar.style.gap = window_width / 8 + "px";
 
-    change_proportions(calendar_button);
-    change_proportions(map_button);
-    change_proportions(settings_button);
+    change_buttons(calendar_button);
+    change_buttons(map_button);
+    change_buttons(settings_button);
+
+    friends_bar.style.height = window_height / 2 + "px";
+
 })
 
-function change_proportions(button) {
-    button.style.height = window_height / 8 -  + 'px';
+function change_buttons(button) {
+    button.style.height = window_height / 8 - +'px';
     button.style.width = window_width / 4 + "px";
 }
