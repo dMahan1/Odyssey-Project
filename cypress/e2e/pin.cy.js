617888640157998f6322654ffee4a4da7890b89a
Player48 <139013669+Player48@users.noreply.github.com>
2026-04-14 16:19:38 -0400

    Pin Test Functional

--- cypress/e2e/pin.cy.js
+++ cypress/e2e/pin.cy.js
@@ -1,4 +1,5 @@
 describe('map action', () => {
+    const PIN_SELECTOR = 'img.leaflet-marker-icon[src*="marker-icon-2x.png"]';
     it('pin', () => {
         cy.visit('/');
         cy.stubGeolocation();
@@ -11,6 +12,13 @@ describe('map action', () => {
         cy.url().should('include', 'Map.html');
         cy.get('#make-pin-btn').click();
         cy.get('#map').click(500, 300);
-        
+        cy.get('#temp-pin-name').type('Test');
+        cy.get('#save-temp-pin').click();
+        cy.get(PIN_SELECTOR).should('exist').and('be.visible');
+        cy.get(PIN_SELECTOR).trigger('click', { force: true });
+        cy.get('.leaflet-popup-content strong').should('contain', 'Test');
+        cy.get('.pull-pin-btn').click();
+        cy.get('.leaflet-popup-content').should('not.exist');
+        cy.get(PIN_SELECTOR).should('not.exist');
     });
 });
